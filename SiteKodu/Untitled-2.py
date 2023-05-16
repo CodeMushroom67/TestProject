8e92b162c1a409f2e5dfa8285e490046f2241791
CodeMushroom67 <131439485+CodeMushroom67@users.noreply.github.com>
2023-05-16 21:51:51 +0300

    Delete SiteKodu directory

--- SiteKodu/Untitled-2.py
+++
@@ -1,5 +0,0 @@
-erdogan = input("Erdogan: ")
-kemal = input("Kemal: ")
-sinan = input("Sinan: ")
-sonuc = "Erdogan:%" + erdogan + "     Kiliçlaroğlu:%" + kemal + "     Sinan:%" + sinan
-print(sonuc)

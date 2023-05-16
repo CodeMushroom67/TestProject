6c9135dba59164b87b2516b717abcdfbf748ee02
CodeMushroom67 <131439485+CodeMushroom67@users.noreply.github.com>
2023-05-16 21:53:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("scroll", (event) => {
+    console.log(document.body.scrollTop);
+    if (document.body.scrollTop > 250) {
+        document.getElementById("mask1").style.left = "-5px";
+
+    } else {
+        document.getElementById("mask1").style.left = "-590px";
+    }
+    if (document.body.scrollTop > 250) {
+        document.getElementById("mask2").style.right = "-120vw";
+
+    } else {
+        document.getElementById("mask2").style.right = "-145vw";
+    }
+    if (document.body.scrollTop > 250) {
+        document.getElementById("mask3").style.left = "-5px";
+
+    } else {
+        document.getElementById("mask3").style.left = "-590px";
+    }
+    if (document.body.scrollTop > 250) {
+        document.getElementById("TheTitle").style.opacity = 1;
+
+    } else {
+        document.getElementById("TheTitle").style.opacity = 0;
+    }
+})
